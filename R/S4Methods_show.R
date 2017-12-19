a2e635111bacaf29b75337bea64743eebc8cd91e
iferres <igferres@gmail.com>
2017-12-19 20:55:16 -0300

    added show method

--- R/S4Methods_show.R
+++ R/S4Methods_show.R
@@ -6,3 +6,21 @@
 #             cat("GPA:", object@GPA, "\n")
 #           }
 # )
+
+# prokka
+setMethod('show',
+          'prokka',
+          function(object){
+            cat('An object of class "prokka"\n')
+            x <- object@in.files
+            p <- paste('Annotation for', length(x), 'genomes:\n ')
+            cat(p)
+            if(length(x)>4){
+              cat(x[1:2], sep = '\n ')
+              cat(' ...\n ')
+              cat(rev(x)[1])
+            }else{
+              cat(x, sep = '\n ')
+            }
+          }
+)
