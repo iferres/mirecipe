321a5f4ac6a381979e89e6d3c644f0f414162599
iferres <igferres@gmail.com>
2017-12-19 20:55:29 -0300

    added summary method

--- R/S4Methods_summary.R
+++ R/S4Methods_summary.R
@@ -5,3 +5,17 @@
 #   "I'm a foo"
 # }
 # setMethod("summary", "foo", summary.foo)
+
+#Just once!
+setGeneric('summary')
+
+
+#Set S3 Method
+summary.prokka <- function(object){
+  
+  object@stats
+  
+}
+
+#Set S4 Method
+setMethod('summary', 'prokka', summary.prokka)
