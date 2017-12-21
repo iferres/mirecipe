db25454232ca039578dbeb5d5f4aefb079a5351f
iferres <igferres@gmail.com>
2017-12-21 18:43:04 -0300

    finished progressiveMauve, first try

--- R/progressiveMauve.R
+++ R/progressiveMauve.R
@@ -5,7 +5,7 @@ setClass('progressiveMauve',
                       stats = 'data.frame',
                       call = 'character', 
                       prefix = 'character',
-                      panmatrix = 'matrix'), 
+                      shared.percent = 'matrix'), 
          validity = function(object){
            val <- sapply(slot(object, 'out.files'), 
                          sapply, 
@@ -87,7 +87,7 @@ progressiveMauve <- function(prokka,
              value = TRUE)
   bk <- read.csv(gp, sep = '\t', header = TRUE)
   
-  
+  m <- pcSharedGenome(prokka = prokka, backbone = bk)
   
   res <- new('progressiveMauve', 
              in.files = in.files, 
@@ -95,7 +95,7 @@ progressiveMauve <- function(prokka,
              stats = stats, 
              call = run,
              prefix = prefix, 
-             panmatrix = panmatrix)
+             shared.percent = m)
   
 }
 
@@ -106,40 +106,36 @@ progressiveMauve <- function(prokka,
 pcSharedGenome <- function(prokka, backbone){
   
   in.prokka <- slot(prokka, 'in.files')
-  
+
   gnam <- sapply(strsplit(in.prokka, '/'), function(x) x[length(x)] )
-  
   len <- slot(prokka, 'stats')$bases
-  
   names(len) <- gnam
   
   sb <- backbone[, 2*(1:length(in.prokka))]
   
   cb <- combn(length(in.prokka), 2L)
   
-  sh <- apply(cb, 2, function(x)  which(sb[,x[1]]>0 & sb[,x[2]]>0))
+  sh <- apply(cb, 2, function(x){
+    which(sb[,x[1]]>0 & sb[,x[2]]>0)
+    })
   
   su <- abs(backbone[, c(T,F)] - backbone[, c(F,T)])
   
   sha <- lapply(1:length(in.prokka), function(x){
-    
     a <- (sum(su[sh[[x]], cb[1L, x]])/len[ cb[1L, x] ]) * 100 
-    
     b <- (sum(su[sh[[x]], cb[2L, x]])/len[ cb[2L, x] ]) * 100
-    
     c(a, b)
   })
   
-  m <- matrix(100, nrow = length(in.prokka), ncol = length(in.prokka))
+  m <- matrix(100, 
+              nrow = length(in.prokka), 
+              ncol = length(in.prokka))
   colnames(m) <- gnam -> rownames(m)
   
   for (i in 1:length(sha)){
-    
     m[names(sha[[i]])[1], names(sha[[i]])[2]] <- sha[[i]][1]
-    
     m[names(sha[[i]])[2], names(sha[[i]])[1]] <- sha[[i]][2]
-    
-  }
+    }
   
   return(m)
 }
